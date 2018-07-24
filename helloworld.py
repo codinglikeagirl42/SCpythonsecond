d4d22d034ea448064b406acdbca5affa08d767cd
Kimberly Horan <codinglikeagirl42@gmail.com>
2018-07-24 14:22:07 +0000

    first program

---
+++ helloworld.py
@@ -0,0 +1,16 @@
+#First computer program
+
+print("Hello World")
+print("Its a beautiful day in the neighborhood")
+
+print 2+4
+print 2+(4*7)
+print 2**2
+print 10%3
+
+x = 3
+y = 2
+if x == y:
+    print ("Yay")
+else:
+    print ("Nay") + y
